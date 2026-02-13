316a7d08262a368615f3fad5e7036e6410bd4732
Gabe Chantayan <garnet638@gmail.com>
2026-02-12 21:35:40 -0500

    Initial Commit

---
+++ dzen/dzenApp.swift
@@ -0,0 +1,17 @@
+//
+//  dzenApp.swift
+//  dzen
+//
+//  Created by Gabe Chantayan on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct dzenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
